1aa103b244159dc072749a73642df92e14a18854
COBRAXDOP <85988755+COBRAXDOP@users.noreply.github.com>
2021-09-17 21:01:11 +0530

    Update musicplayer.py

--- MashaRoBot/modules/musicplayer.py
+++ MashaRoBot/modules/musicplayer.py
@@ -31,7 +31,7 @@ help = """
   /playlist: Shows playlist
 =>> More tools 📲
   /admincache: Updates admin info of your group. Try if bot isn't recognize admin
-  /userbotjoin: Invite @LOVELYVC Userbot to your chat
+  /userbotjoin: Invite @Aliza_Assistant Userbot to your chat
 *Player cmd and all other cmds except /play, /current  and /playlist  are only for admins with manage group
 <b> PLEASE NOTE THIS SERVICE IS UNSTABLE AND CAN BE STOPPED ANYTIME </b>
 """
